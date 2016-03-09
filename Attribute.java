bfde0c440ef378e73399409e55d42d860ae82f20
Natalie Keelan <nataliekeelan@pcp132173pcs.wireless.calpoly.edu>
2016-03-08 17:43:58 -0800

    Changes to Attribute.

--- Attribute.java
+++ Attribute.java
@@ -4,6 +4,12 @@ public class Attribute
    private int importance;
    private int value;
 
+   public Attribute(String name, int value, int importance) {
+      this.name = name;
+      this.value = value;
+      this.importance = importance;
+   }
+
    public String getAttributeName()
    {
       return name;
