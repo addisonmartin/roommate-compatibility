f862221f6e0bdd481f9ac08c23b9ea0171a2f64b
Addison Martin <addisonejmartin@gmail.com>
2016-03-10 17:43:51 -0800

    Fixed rounding

--- RandomCompatibilityTest.java
+++ RandomCompatibilityTest.java
@@ -1,6 +1,6 @@
 public class RandomCompatibilityTest implements CompatibilityTest
 {
    public double calculate(Person person1, Person person2) {
-      return (Math.random() * 100);
+      return (double) (Math.round((Math.random() * 100) * 100) / 100);
    }
 }
